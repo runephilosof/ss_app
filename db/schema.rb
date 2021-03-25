1fb77ecd46c14f4143c7abfa6e61dbd28c5d973f
Rune Philosof <rp@abtion.com>
2021-03-25 06:51:34 +0000

    Rerunning the migrations
    ```
    bin/rails db:drop db:create db:migrate
    ```
    
    Gives a slightly different `schema.rb`.
    Rerunning the migrations should give identical `schema.rb` in a project such as this one.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,10 +13,10 @@
 ActiveRecord::Schema.define(version: 20150202011847) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",      limit: 4000
-    t.string   "name",       limit: 4000
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "email"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
